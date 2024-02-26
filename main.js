4ed0c0342d56198af8df3ac9928bf2755edf3938
ssparshsethi <142225493+ssparshsethi@users.noreply.github.com>
2024-02-26 12:59:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function preload() {
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+}
